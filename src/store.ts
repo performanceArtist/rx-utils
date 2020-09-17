216456bcccc20cd2111040ef2f798ca0e0b37d09
megakaban <nakedant19@gmail.com>
2020-09-17 15:45:04 +0700

    Remove arrayStore, add tests

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { option, array, either } from 'fp-ts';
+import { option, either } from 'fp-ts';
 import { Option } from 'fp-ts/lib/Option';
 import { pipe } from 'fp-ts/lib/pipeable';
 import { flow, constVoid } from 'fp-ts/lib/function';
@@ -8,7 +8,7 @@ import { flow, constVoid } from 'fp-ts/lib/function';
 export type Handlers<T, K, V> = {
   empty: T;
   get: (key: K, store: T) => Option<V>;
-  set: (key: K, value: V, store: T) => Option<T>;
+  set: (key: K, value: V, store: T) => T;
   remove: (key: K, store: T) => Option<T>;
 };
 
@@ -20,24 +20,35 @@ export type Store<K, V, T> = {
   setAll: (values: T) => void;
   remove: (key: K) => Option<T>;
   has: (key: K) => boolean;
-  modify: (key: K, update: (value: V) => V) => void;
+  modify: (key: K, update: (value: V) => V) => Option<T>;
   modifyAll: (update: (value: T) => T) => void;
   all$: Observable<T>;
+};
+
+export const createBehavior = <T>(initial: T) => {
+  const subject = new BehaviorSubject<T>(initial);
+  const $ = subject.asObservable();
+  const get = subject.getValue.bind(subject);
+  const set = subject.next.bind(subject);
+  const modify = (update: (value: T) => T) => set(update(get()));
+
+  return {
+    get,
+    set,
+    modify,
+    $,
+  };
 };
 
 export const makeStore = <T, K, V>(
   handlers: Handlers<T, K, V>,
 ): Store<K, V, T> => {
-  const store = new BehaviorSubject(handlers.empty);
+  const store = createBehavior(handlers.empty);
 
-  const all$ = store.asObservable();
-  const getAll = store.getValue.bind(store);
-  const setAll = store.next.bind(store);
-
-  const get = (key: K) => handlers.get(key, store.getValue());
+  const get = (key: K) => handlers.get(key, store.get());
 
   const set = (key: K, value: V) =>
-    pipe(handlers.set(key, value, store.getValue()), option.map(setAll));
+    pipe(handlers.set(key, value, store.get()), store.set);
 
   const getOrElse = (key: K, provide: () => V): V =>
     pipe(
@@ -50,41 +61,35 @@ export const makeStore = <T, K, V>(
     );
 
   const modify = (key: K, update: (value: V) => V) =>
-    pipe(get(key), option.map(flow(update, value => set(key, value))));
-
-  const modifyAll = (update: (value: T) => T) => setAll(update(getAll()));
+    pipe(
+      get(key),
+      option.map(flow(update, value => set(key, value))),
+      option.map(() => store.get()),
+    );
 
   const has = (key: K) => pipe(get(key), option.isSome);
 
-  const remove = (key: K) => handlers.remove(key, getAll());
+  const remove = (key: K) => handlers.remove(key, store.get());
 
   return {
     get,
-    getAll,
+    getAll: store.get,
     getOrElse,
     set,
-    setAll,
+    setAll: store.set,
     remove,
     has,
     modify,
-    modifyAll,
-    all$,
+    modifyAll: store.modify,
+    all$: store.$,
   };
 };
-
-export const arrayStore = <T>() =>
-  makeStore<T[], number, T>({
-    empty: array.empty,
-    get: array.lookup,
-    set: (key, value, store) => pipe(store, array.updateAt(key, value)),
-    remove: (key, store) => pipe(store, array.deleteAt(key)),
-  });
 
 export const mapStore = <K, V>() =>
   makeStore<Map<K, V>, K, V>({
     empty: new Map(),
     get: (key, store) => option.fromNullable(store.get(key)),
-    set: (key, value, store) => option.some(store.set(key, value)),
+    set: (key, value, store) => store.set(key, value),
     remove: (key, store) =>
       store.delete(key) ? option.some(store) : option.none,
   });
